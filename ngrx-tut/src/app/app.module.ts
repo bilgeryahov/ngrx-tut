fcd8832f6b28ceec5826fe71db43cd1c7cd15a08
bilgeryahov <bayahov1@gmail.com>
2019-10-21 17:55:22 +0300

    Initialize the Store and declare the newly created components.

--- ngrx-tut/src/app/app.module.ts
+++ ngrx-tut/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/tutorial.reducer';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
