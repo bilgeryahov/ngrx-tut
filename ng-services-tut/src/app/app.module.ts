f67d17e3a15da6e9bf6fe409442bd1c21aedd380
bilgeryahov <bayahov1@gmail.com>
2019-10-22 17:56:10 +0300

    Import and use components and services from the App module.

--- ng-services-tut/src/app/app.module.ts
+++ ng-services-tut/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
+
+import { TutorialService } from './services/tutorial.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TutorialService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
